16894fa7e7301db8af41c4ce01c2e2593210c72e
Lexi <lexibu007@gmail.com>
2022-03-03 21:20:14 -0600

    Added popular lines of business component

---
+++ src/app/popular-lines-of-business/popular-lines-of-business.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-popular-lines-of-business',
+  templateUrl: './popular-lines-of-business.component.html',
+  styleUrls: ['./popular-lines-of-business.component.css']
+})
+export class PopularLinesOfBusinessComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
