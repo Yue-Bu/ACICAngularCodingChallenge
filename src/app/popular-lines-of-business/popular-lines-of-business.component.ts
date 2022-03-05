739cec8f2f81e03da5f8fed6ad7f35e903b2df70
Lexi <lexibu007@gmail.com>
2022-03-05 11:16:52 -0600

    Updated popular-lines-of-business component

--- src/app/popular-lines-of-business/popular-lines-of-business.component.ts
+++ src/app/popular-lines-of-business/popular-lines-of-business.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LineOfBusiness } from '../LineOfBusiness';
+import { LineOfBusinessService } from '../lineOfBusiness.service';
 
 @Component({
   selector: 'app-popular-lines-of-business',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PopularLinesOfBusinessComponent implements OnInit {
 
-  constructor() { }
+  linesOfBusiness: LineOfBusiness[] = [];
 
-  ngOnInit(): void {
+  constructor(private lineOfBusinessService: LineOfBusinessService) { }
+
+  ngOnInit() {
+    this.getLinesOfBusiness();
+  }
+
+  getLinesOfBusiness(): void {
+    this.lineOfBusinessService.getLinesOfBusiness()
+      .subscribe(linesOfBusiness => this.linesOfBusiness = linesOfBusiness.slice(1, 4));
   }
 
 }
